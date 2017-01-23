4a734b1ac7f166dab35055bbb637481968b43f4b
David Moon <dmoon@fetchrobotics.com>
2017-01-22 21:05:07 -0800

    Refactored 5 playCard functions and added 4 bugs.

--- projects/moondav/dominion/dominion.h
+++ projects/moondav/dominion/dominion.h
@@ -9,6 +9,8 @@
 #define MAX_PLAYERS 4
 
 #define DEBUG 0
+#define TRUE 1
+#define FALSE 0
 
 
 
@@ -130,11 +132,11 @@ int getWinners(int players[MAX_PLAYERS], struct gameState *state);
 /* Set array position of each player who won (remember ties!) to
    1, others to 0 */
 
-int playAdventurer(struct gameState *state);
+int playAdventurer(struct gameState *state, int handPos);
 int playSmithy(struct gameState *state, int handPos);
 int playVillage(struct gameState *state, int handPos);
-int playFeast(struct gameState *state, int choice1);
-int playCouncil_Room(struct gameState *state, int handPos);
+int playFeast(struct gameState *state, int handPos, int choice1);
+int playCouncilRoom(struct gameState *state, int handPos);
 
 
 #endif
