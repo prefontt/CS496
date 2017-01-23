20f95a4205266c9d5860a21e9ddd9714b2c8621b
Patrick Levy <levyp@oregonstate.edu>
2017-01-22 22:38:31 -0600

    Refactored the code for playing the baron card into a separate function and performed a lot of cleanup to make it more readable.

--- projects/levyp/dominion/dominion.c
+++ projects/levyp/dominion/dominion.c
@@ -830,54 +830,7 @@ int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState
       return 0;
 		
     case baron:
-      state->numBuys++;//Increase buys by 1!
-      if (choice1 > 0){//Boolean true or going to discard an estate
-	int p = 0;//Iterator for hand!
-	int card_not_discarded = 1;//Flag for discard set!
-	while(card_not_discarded){
-	  if (state->hand[currentPlayer][p] == estate){//Found an estate card!
-	    state->coins += 4;//Add 4 coins to the amount of coins
-	    state->discard[currentPlayer][state->discardCount[currentPlayer]] = state->hand[currentPlayer][p];
-	    state->discardCount[currentPlayer]++;
-	    for (;p < state->handCount[currentPlayer]; p++){
-	      state->hand[currentPlayer][p] = state->hand[currentPlayer][p+1];
-	    }
-	    state->hand[currentPlayer][state->handCount[currentPlayer]] = -1;
-	    state->handCount[currentPlayer]--;
-	    card_not_discarded = 0;//Exit the loop
-	  }
-	  else if (p > state->handCount[currentPlayer]){
-	    if(DEBUG) {
-	      printf("No estate cards in your hand, invalid choice\n");
-	      printf("Must gain an estate if there are any\n");
-	    }
-	    if (supplyCount(estate, state) > 0){
-	      gainCard(estate, state, 0, currentPlayer);
-	      state->supplyCount[estate]--;//Decrement estates
-	      if (supplyCount(estate, state) == 0){
-		isGameOver(state);
-	      }
-	    }
-	    card_not_discarded = 0;//Exit the loop
-	  }
-			    
-	  else{
-	    p++;//Next card
-	  }
-	}
-      }
-			    
-      else{
-	if (supplyCount(estate, state) > 0){
-	  gainCard(estate, state, 0, currentPlayer);//Gain an estate
-	  state->supplyCount[estate]--;//Decrement Estates
-	  if (supplyCount(estate, state) == 0){
-	    isGameOver(state);
-	  }
-	}
-      }
-	    
-      
+      playBaron(currentPlayer, state, choice1);    
       return 0;
 		
     case great_hall:
@@ -1347,9 +1300,56 @@ int playAdventurer(int currentPlayer, struct gameState *state) {
   return 0;
 }
 
+// Baron
+int playBaron(int currentPlayer, struct gameState *state, int choice1) {
+  state->numBuys++;//Increase buys by 1!
+  if (choice1 > 0) {//Boolean true or going to discard an estate
+    int p = 0;//Iterator for hand!
+    int card_not_discarded = 1;//Flag for discard set!
+    while(card_not_discarded){
+      if (state->hand[currentPlayer][p] == estate){//Found an estate card!
+        state->coins += 4;//Add 4 coins to the amount of coins
+        state->discard[currentPlayer][state->discardCount[currentPlayer]] = state->hand[currentPlayer][p];
+        state->discardCount[currentPlayer]++;
+        for (;p < state->handCount[currentPlayer]; p++){
+          state->hand[currentPlayer][p] = state->hand[currentPlayer][p+1];
+        }
+        state->hand[currentPlayer][state->handCount[currentPlayer]] = -1;
+        state->handCount[currentPlayer]--;
+        card_not_discarded = 0;//Exit the loop
+      }
+      else if (p > state->handCount[currentPlayer]){
+        if(DEBUG) {
+          printf("No estate cards in your hand, invalid choice\n");
+          printf("Must gain an estate if there are any\n");
+        }
+        if (supplyCount(estate, state) > 0){
+          gainCard(estate, state, 0, currentPlayer);
+          state->supplyCount[estate]--;//Decrement estates
+          if (supplyCount(estate, state) == 0){
+            isGameOver(state);
+          }
+        }
+        card_not_discarded = 0;//Exit the loop
+      }
+            
+      else{
+        p++;//Next card
+      }
+    }
+  }        
+  else {
+    if (supplyCount(estate, state) > 0){
+      gainCard(estate, state, 0, currentPlayer);//Gain an estate
+      state->supplyCount[estate]--;//Decrement Estates
+      if (supplyCount(estate, state) == 0){
+        isGameOver(state);
+      }
+    }
+  }
+  return 0;
+}
 
-
-// Card 3
   
 // Card 4
 
--- projects/levyp/dominion/dominion.h
+++ projects/levyp/dominion/dominion.h
@@ -137,5 +137,6 @@ int playCouncil_Room(struct gameState *state, int handPos);
 // My new declarations
 int playSmithy(int player, struct gameState *state);
 int playAdventurer(int currentPlayer, struct gameState *state);
+int playBaron(int currentPlayer, struct gameState *state, int choice1);
 
 #endif
