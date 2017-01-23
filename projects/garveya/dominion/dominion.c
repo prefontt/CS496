2f858d4091bcda57878a7b356a6e39dde1117f82
Allen Garvey <allen.garvey@gmail.com>
2017-01-23 00:12:37 -0500

    Add bug to smithy card function

--- projects/garveya/dominion/dominion.c
+++ projects/garveya/dominion/dominion.c
@@ -1329,9 +1329,8 @@ int playAdventurer(int currentPlayer, struct gameState *state){
 int playSmithy(int currentPlayer, struct gameState *state, int handPos){
   int i;
   //+3 Cards
-  for (i = 0; i < 3; i++){
-    drawCard(currentPlayer, state);
-  }
+  for (i = 0; i < 3; i++);                                                                                                                                              ;
+  drawCard(currentPlayer, state);
   //discard card from hand
   discardCard(handPos, currentPlayer, state, 0);
   return 0;
