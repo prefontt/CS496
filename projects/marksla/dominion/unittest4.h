3b43b3ddb45752f25dca6b9dbc53b2e1477f1277
Lance Marks <marksla@flip3.engr.oregonstate.edu>
2017-01-31 21:45:03 -0800

    UnitTest1 completed, helperfunctions fixed

---
+++ projects/marksla/dominion/unittest4.h
@@ -0,0 +1,12 @@
+/*************************************************************************
+ * Author: Lance Marks marksla
+ * Date: 20170205
+ * Class: CS362 Software Development II
+ * Description: Header File
+ * **********************************************************************/
+#ifndef _UNITTEST4_H
+#define _UNITTEST4_H
+
+
+#endif
+
