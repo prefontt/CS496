d00e17e4677aa3a70d5b52665bd7592c48a09eec
Lance Marks <marksla@flip2.engr.oregonstate.edu>
2017-02-01 22:36:04 -0800

    Finished cardtest1. Edited testhelperfunctions to remove broken functions.

--- projects/marksla/dominion/testhelperfunctions.h
+++ projects/marksla/dominion/testhelperfunctions.h
@@ -3,12 +3,6 @@
 
 #include "dominion.h"
 
-int copyArray(int *originalArray, int *copyArray, int cols);
-
-int copy2DArray(int **originalArray, int **copyArray, int rows, int cols);
-
-int copyGameState(struct gameState *original, struct gameState *copy);
-
 int fillPlayerHand(int player, int card, int count, struct gameState *state);
 
 int placePlayerHand(int player, int card, int position, struct gameState *state);
