e288880083fc17a261a48a117ef048d190b2185d
jz0425 <zhaojing@oregonstat.edu>
2017-02-04 01:23:13 -0800

    Assignment #3
    1) Heavy revisions to makefile
    2) Minor revisions to 2 unittest files
    3) Generated new unittestresult.out

--- projects/zhaojing/dominion/unittest2.c
+++ projects/zhaojing/dominion/unittest2.c
@@ -13,36 +13,38 @@ int main(int argc, char ** argv){
 	int result, randomVar = 0;
 	int k[10] = {adventurer, council_room, feast, gardens, mine, remodel, smithy, village, baron, great_hall};
 	randomVar = rand() % 10;
-	result = initializeGame(2, k, randomVar, &G);
-	int i, j;
+	initializeGame(2, k, randomVar, &G);
+	
+	int i;
 	
 	//test for whether game ends when there are province and supply cards
 	printf("testing non-empty province and supply stacks\n");
+	
 	G.supplyCount[province] == 5;
 	for(i = 0; i < 25; i++) G.supplyCount[i] = 1;
-	result = isGameOver(&G);
-	asserttrue(result == 0);
+	//result = isGameOver(&G);
+	asserttrue(isGameOver(&G) == 0);
 	
 	//test for whether game ends when only province stack is empty
 	printf("testing empty province stack\n");
 	G.supplyCount[province] == 0;
 	for(i = 0; i < 25; i++) G.supplyCount[i] = 1;
-	result = isGameOver(&G);
-	asserttrue(result == 1);
+	//result = isGameOver(&G);
+	asserttrue(isGameOver(&G) == 1);
 	
 	//test for whether game ends when only 3 supply piles are empty
 	printf("testing empty supply stacks\n");
 	G.supplyCount[province] == 3;
 	for(i = 0; i < 25; i++) G.supplyCount[i] = 0;
-	result = isGameOver(&G);
-	asserttrue(result == 1);
+	//result = isGameOver(&G);
+	asserttrue(isGameOver(&G) == 1);
 	
 	//test for whether game ends when both province and supply are empty
 	printf("testing empty province and supply stacks\n");
 	G.supplyCount[province] == 0;
 	for(i = 0; i < 25; i++) G.supplyCount[i] = 0;
-	result = isGameOver(&G);
-	asserttrue(result == 1);
+	//result = isGameOver(&G);
+	asserttrue(isGameOver(&G) == 1);
 	
 	printf("******end unit test on isGameOver()******\n");
 	return 0;
