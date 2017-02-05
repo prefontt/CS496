2f4d5a51bcfcf6c74cf0e7d9131d6257f6161cb7
Beth Poore <beth.poore@gmail.com>
2017-02-04 16:42:41 -0800

    Add quiz 2 files

--- projects/pooree/dominion/dominion.c
+++ projects/pooree/dominion/dominion.c
@@ -1131,34 +1131,30 @@ int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState
     case treasure_map:
       //search hand for another treasure_map
       index = -1;
-      for (i = 0; i < state->handCount[currentPlayer]; i++)
-	{
-	  if (state->hand[currentPlayer][i] == treasure_map && i != handPos)
-	    {
+      for (i = 0; i < state->handCount[currentPlayer]; i++){
+	     if (state->hand[currentPlayer][i] == treasure_map && i != handPos){
 	      index = i;
 	      break;
 	    }
-	}
-      if (index > -1)
-	{
-	  //trash both treasure cards
-	  discardCard(handPos, currentPlayer, state, 1);
-	  discardCard(index, currentPlayer, state, 1);
+	   }
+    if (index > -1){
+  	  //trash both treasure cards
+  	  discardCard(handPos, currentPlayer, state, 1);
+  	  discardCard(index, currentPlayer, state, 1);
 
-	  //gain 4 Gold cards
-	  for (i = 0; i < 4; i++)
-	    {
-	      gainCard(gold, state, 1, currentPlayer);
-	    }
-				
-	  //return success
-	  return 1;
-	}
-			
+  	  //gain 4 Gold cards
+  	  for (i = 0; i < 4; i++)
+  	    {
+  	      gainCard(gold, state, 1, currentPlayer);
+  	    }
+  				
+  	  //return success
+  	  return 1;
+  	}	
       //no second treasure_map found in hand
       return -1;
     }
-	
+    
   return -1;
 }
 
