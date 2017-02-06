d7829deaf0d9b39b0e10f24015352fe485ff14c2
left brain <brittneymcinnis1@Brittneys-MacBook-Pro.local>
2017-02-05 19:13:27 -0600

    random testing quiz 2

---
+++ projects/mcinnisb/randomstring.c
@@ -0,0 +1,13 @@
+/*******************************************************************************
+ * The inputChar() function has an array of the characters {'[', '(', '{', ' ',
+ * 'a', 'x', ')','}', ']', 'z'}. A random number between 0 and 9 is chosen to 
+ * pick the index of the character returned from the function ('z' is included in
+ * the array, since this will be false for all of the if conditionals).
+ *
+ * The inputString() function has an array of strings {"reset", "rrr", "foo", "bar",
+ * "baz", "figaro", "hello", "world", "rese", "eset"}. A random number between 0 and
+ * 9 is chosen to pick the index of the character returned from the function
+ * (the more strings in the array, the less likely it will be that "reset" is
+ * chosen at the same time that state = 9, with the effect being that there will
+ * be more iterations of the while loop.
+ *******************************************************************************/
